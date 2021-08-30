b1b24d8545834b25bb873c0c6d392e4e73f0b52a
cdrovandi <c.drovandi@qut.edu.au>
2021-08-30 18:51:23 +1000

    Delete simulate_ma2.m

--- simulate_ma2.m
+++
@@ -1,8 +0,0 @@
-function y = simulate_ma2(theta,T,reps)
-    % simulate MA(2) time series model.  The function allows to simulate
-    % for than one independent replicate via the reps parameter
-    e = randn(T+2,reps);
-    y = e(3:end,:) + theta(1)*e(2:end-1,:) + theta(2)*e(1:end-2,:);
-end
-
-
